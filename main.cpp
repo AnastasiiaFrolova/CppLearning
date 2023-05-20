8c855db2c44e16f0b155833a006337907ef4c57a
Анастасия <FrolNastya10@mail.ru>
2023-05-20 19:26:16 +0400

    работа 2

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@
 std::vector<std::string> split(const std::string& str, char d)
 {
 	std::vector<std::string> r;
-
 	std::string::size_type start = 0;
 	std::string::size_type stop = str.find_first_of(d);
 	while (stop != std::string::npos)
