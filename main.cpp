ef3f5524bf25d5e7da876df05873588fe441dc3b
Анастасия <FrolNastya10@mail.ru>
2023-05-20 18:12:02 +0400

    Работа 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,155 @@
-#include "lib.h"
-
+#include <cassert>
+#include <cstdlib>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
 
-int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Helloooo, world!" << std::endl;
-    return 0;
+
+// ("",  '.') -> [""]
+// ("11", '.') -> ["11"]
+// ("..", '.') -> ["", "", ""]
+// ("11.", '.') -> ["11", ""]
+// (".11", '.') -> ["", "11"]
+// ("11.22", '.') -> ["11", "22"]
+std::vector<std::string> split(const std::string& str, char d)
+{
+	std::vector<std::string> r;
+
+	std::string::size_type start = 0;
+	std::string::size_type stop = str.find_first_of(d);
+	while (stop != std::string::npos)
+	{
+		r.push_back(str.substr(start, stop - start));
+
+		start = stop + 1;
+		stop = str.find_first_of(d, start);
+	}
+
+	r.push_back(str.substr(start));
+
+	return r;
+}
+
+void reverseSort(std::vector<std::tuple<std::string, std::string, std::string, std::string>>& data) {
+	for (size_t idx_i = 0; idx_i + 1 < data.size(); ++idx_i) {
+		for (size_t idx_j = 0; idx_j + 1 < data.size() - idx_i; ++idx_j) {
+			if (stoi(std::get<0>(data[idx_j + 1])) > stoi(std::get<0>(data[idx_j]))) {
+				swap(data[idx_j], data[idx_j + 1]);
+			}
+			if (std::get<0>(data[idx_j + 1]) == std::get<0>(data[idx_j]) &&
+				stoi(std::get<1>(data[idx_j + 1])) > stoi(std::get<1>(data[idx_j]))) {
+				swap(data[idx_j], data[idx_j + 1]);
+			}
+			if (std::get<0>(data[idx_j + 1]) == std::get<0>(data[idx_j]) &&
+				std::get<1>(data[idx_j + 1]) == std::get<1>(data[idx_j]) &&
+				stoi(std::get<2>(data[idx_j + 1])) > stoi(std::get<2>(data[idx_j]))) {
+				swap(data[idx_j], data[idx_j + 1]);
+			}
+			if (std::get<0>(data[idx_j + 1]) == std::get<0>(data[idx_j]) &&
+				std::get<1>(data[idx_j + 1]) == std::get<1>(data[idx_j]) &&
+				std::get<2>(data[idx_j + 1]) == std::get<2>(data[idx_j]) &&
+				stoi(std::get<3>(data[idx_j + 1])) > stoi(std::get<3>(data[idx_j]))) {
+				swap(data[idx_j], data[idx_j + 1]);
+			}
+		}
+	}
+}
+
+int main(int argc, char const* argv[])
+{
+	try
+	{
+		std::vector<std::tuple<std::string, std::string, std::string, std::string>> ip_pool;
+		auto parseStr = [](const std::string& str, char d)
+		{ std::vector<std::string> v = split(str, d); return std::make_tuple(v); };
+
+		for (std::string line; std::getline(std::cin, line);)
+		{
+			if (line.empty()) break;
+
+			auto ip = std::get<0>(parseStr(line, '\t'))[0];
+			auto ipParts = std::get<0>(parseStr(ip, '.'));
+			ip_pool.push_back(std::make_tuple(ipParts[0], ipParts[1], ipParts[2], ipParts[3]));
+		}
+		reverseSort(ip_pool);
+
+
+		// TODO reverse lexicographically sort
+
+		for (std::vector<std::tuple<std::string, std::string, std::string, std::string>>::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+		{
+			std::cout << std::get<0>(*ip) << "." << std::get<1>(*ip) << "." << std::get<2>(*ip) << "." << std::get<3>(*ip) << std::endl;
+
+		}
+
+		// 222.173.235.246
+		// 222.130.177.64
+		// 222.82.198.61
+		// ...
+		// 1.70.44.170
+		// 1.29.168.152
+		// 1.1.234.8
+
+		// TODO filter by first byte and output
+		// ip = filter(1)
+
+		// 1.231.69.33
+		// 1.87.203.225
+		// 1.70.44.170
+		// 1.29.168.152
+		// 1.1.234.8
+
+		// TODO filter by first and second bytes and output
+		// ip = filter(46, 70)
+
+		// 46.70.225.39
+		// 46.70.147.26
+		// 46.70.113.73
+		// 46.70.29.76
+
+		// TODO filter by any byte and output
+		// ip = filter_any(46)
+
+		// 186.204.34.46
+		// 186.46.222.194
+		// 185.46.87.231
+		// 185.46.86.132
+		// 185.46.86.131
+		// 185.46.86.131
+		// 185.46.86.22
+		// 185.46.85.204
+		// 185.46.85.78
+		// 68.46.218.208
+		// 46.251.197.23
+		// 46.223.254.56
+		// 46.223.254.56
+		// 46.182.19.219
+		// 46.161.63.66
+		// 46.161.61.51
+		// 46.161.60.92
+		// 46.161.60.35
+		// 46.161.58.202
+		// 46.161.56.241
+		// 46.161.56.203
+		// 46.161.56.174
+		// 46.161.56.106
+		// 46.161.56.106
+		// 46.101.163.119
+		// 46.101.127.145
+		// 46.70.225.39
+		// 46.70.147.26
+		// 46.70.113.73
+		// 46.70.29.76
+		// 46.55.46.98
+		// 46.49.43.85
+		// 39.46.86.85
+		// 5.189.203.46
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
 }
